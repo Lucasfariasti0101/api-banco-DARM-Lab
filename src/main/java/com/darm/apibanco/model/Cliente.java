8c1121cd087e42dbc6a5913bb5f0627dd15841af
Lucas Farias <lucasfariasti0101@gmail.com>
2022-09-18 21:47:18 -0300

    Persistence modifications in the client model

--- src/main/java/com/darm/apibanco/model/Cliente.java
+++ src/main/java/com/darm/apibanco/model/Cliente.java
@@ -1,10 +1,15 @@
 package com.darm.apibanco.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -15,17 +20,35 @@ public class Cliente {
     @Id
     private UUID id;
 
+    @NotBlank
+    @Size(max = 60)
     private String nome;
 
+    @NotBlank
+    @Size(max = 11)
     private String cpf;
 
+    @NotBlank
+    @Size(max = 255)
+    @Email
     private String email;
 
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 
+    @NotBlank
+    @Size(max = 255)
     private String endereco;
 
+    @NotBlank
     private TipoDeConta tipoDaConta;
 
+    @NotBlank
     private String senha;
+
+    private String urlFoto;
+
+    @OneToMany
+    private List<Cartao> cartaos;
 }
