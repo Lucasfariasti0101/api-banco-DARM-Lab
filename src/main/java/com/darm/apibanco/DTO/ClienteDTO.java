691a17732898935f7b36a63f59f84dd80a209341
Lucas Farias <lucasfariasti0101@gmail.com>
2022-09-19 15:02:58 -0300

    Created the DTO package

---
+++ src/main/java/com/darm/apibanco/DTO/ClienteDTO.java
@@ -0,0 +1,26 @@
+package com.darm.apibanco.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClienteDTO {
+
+    private String nome;
+
+    private String cpf;
+
+    private String email;
+
+    private String telefone;
+
+    private String endereco;
+
+    private String tipoDaConta;
+
+    private String senha;
+
+    private String confirSenha;
+
+}
