bb5a0e82d804b0da10a3e7b57efc602958436e37
Lucas Farias <lucasfariasti0101@gmail.com>
2023-04-26 15:23:00 -0300

    feat(PersonRepo): add Person Repository

---
+++ src/main/java/com/darm/apibanco/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.darm.apibanco.repository;
+
+import com.darm.apibanco.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
