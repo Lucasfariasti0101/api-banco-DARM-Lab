1e6d8da12498a40fa94002f1fa73146f3a169a9c
Lucas Farias <lucasfariasti0101@gmail.com>
2023-04-26 14:20:27 -0300

    feat(Role): create end add enum roles to user

--- src/main/java/com/darm/apibanco/model/User.java
+++ src/main/java/com/darm/apibanco/model/User.java
@@ -1,13 +1,16 @@
 package com.darm.apibanco.model;
 
+import com.darm.apibanco.model.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,9 +28,13 @@ public class User implements UserDetails {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
+
     }
 
     @Override
