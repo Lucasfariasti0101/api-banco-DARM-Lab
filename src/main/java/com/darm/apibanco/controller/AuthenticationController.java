8341bc66512b1e92b95feeb87ed6605b105a1c46
Lucas Farias <lucasfariasti0101@gmail.com>
2023-04-26 16:38:38 -0300

    feat(Auth Controller): created authentication controller and add endpoints

---
+++ src/main/java/com/darm/apibanco/controller/AuthenticationController.java
@@ -0,0 +1,42 @@
+package com.darm.apibanco.controller;
+
+import com.darm.apibanco.DTO.AuthenticationRequest;
+import com.darm.apibanco.DTO.AuthenticationResponse;
+import com.darm.apibanco.DTO.RegisterUserRequest;
+import com.darm.apibanco.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+    private final AuthenticationService authService;
+
+    public AuthenticationController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterUserRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/register-admin")
+    public ResponseEntity<AuthenticationResponse> registerAdmin(@RequestBody RegisterUserRequest request) {
+
+        return ResponseEntity.ok(authService.register(request));
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+
+        return ResponseEntity.ok(authService.authenticate(request));
+
+    }
+
+}
