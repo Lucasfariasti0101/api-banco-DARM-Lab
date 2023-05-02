3e0006129b4aa449d66e09ec8fdfbf54af617b4f
Lucas Farias <lucasfariasti0101@gmail.com>
2023-05-02 16:45:10 -0300

    feat(Person Repository): add abstract method find all cards person

--- src/main/java/com/darm/apibanco/repository/PersonRepository.java
+++ src/main/java/com/darm/apibanco/repository/PersonRepository.java
@@ -1,8 +1,12 @@
 package com.darm.apibanco.repository;
 
+import com.darm.apibanco.model.Card;
 import com.darm.apibanco.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    List<Card> findAllCards();
 }
