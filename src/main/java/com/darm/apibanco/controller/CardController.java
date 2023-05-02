1515b6f3c947c8a96e91148bb7c3c737c97bccdc
Lucas Farias <lucasfariasti0101@gmail.com>
2023-05-02 17:33:33 -0300

    feat(Card Controller): created card controller with methods findbyid and save

---
+++ src/main/java/com/darm/apibanco/controller/CardController.java
@@ -0,0 +1,33 @@
+package com.darm.apibanco.controller;
+
+import com.darm.apibanco.DTO.CardRequest;
+import com.darm.apibanco.model.Card;
+import com.darm.apibanco.service.CardService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/cards")
+public class CardController {
+
+    private final CardService cardService;
+
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Card> save(@PathVariable Long id, @RequestBody @Valid CardRequest request) {
+        URI uri = URI.create("/api/v1/cards/");
+        return ResponseEntity.created(uri).body(cardService.save(id, request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Card> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(cardService.findCardById(id));
+    }
+
+}
