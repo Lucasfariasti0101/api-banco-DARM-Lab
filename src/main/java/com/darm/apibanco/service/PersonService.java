e3393f06ea23139ffd6891a08033d8d9a414f2dd
Lucas Farias <lucasfariasti0101@gmail.com>
2023-05-02 16:14:24 -0300

    (Person Service): add person service

---
+++ src/main/java/com/darm/apibanco/service/PersonService.java
@@ -0,0 +1,52 @@
+package com.darm.apibanco.service;
+
+import com.darm.apibanco.DTO.PersonResponse;
+import com.darm.apibanco.DTO.PersonUpdateRequest;
+import com.darm.apibanco.DTO.mapper.person.PersonResponseMapper;
+import com.darm.apibanco.exeption.PersonNotFoundException;
+import com.darm.apibanco.model.Person;
+import com.darm.apibanco.repository.PersonRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+    private final PersonResponseMapper mapper;
+
+    public PersonService(PersonRepository personRepository, PersonResponseMapper mapper) {
+        this.personRepository = personRepository;
+        this.mapper = mapper;
+    }
+
+    public PersonResponse findById(Long id) {
+        Person person = personRepository.findById(id)
+                .orElseThrow(PersonNotFoundException::new);
+
+        return mapper.map(person);
+
+    }
+
+    @Transactional
+    public PersonResponse update(Long id, PersonUpdateRequest request) {
+        Person person = personRepository.findById(id)
+                .orElseThrow(PersonNotFoundException::new);
+
+        person.setFirstName(request.firstName());
+        person.setLastName(request.LastName());
+
+        personRepository.save(person);
+        return mapper.map(person);
+    }
+
+    public List<PersonResponse> findAll(Pageable pageable) {
+        return personRepository.findAll(pageable)
+                .stream()
+                .map(mapper::map)
+                .toList();
+    }
+}
