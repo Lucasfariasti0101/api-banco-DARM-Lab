1ee23334e414c82a5303c42c3896d77d3d6d66bd
Lucas Farias <lucasfariasti0101@gmail.com>
2023-05-30 16:43:53 -0300

    refact(PersonNotFoundException): add default message for personNotFoundException

--- src/main/java/com/darm/apibanco/exeption/PersonNotFoundException.java
+++ src/main/java/com/darm/apibanco/exeption/PersonNotFoundException.java
@@ -1,11 +1,8 @@
 package com.darm.apibanco.exeption;
 
 public class PersonNotFoundException extends RuntimeException {
+
     public PersonNotFoundException() {
-    }
-
-    public PersonNotFoundException(String message) {
-        super(message);
-
+        super("Person not found");
     }
 }
